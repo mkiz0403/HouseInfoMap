e38a8886e5b9f368d7519a1c6fda856f9f17ce2b
Taejeong <mkiz0403@gmail.com>
2024-07-02 14:14:04 +0900

    HouseMap 컴포넌트 임포트

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,21 @@
 import "./App.css";
+import HouseMap from "./map/Map";
+import { useState } from "react";
+import houseData from "./data/houseData.json";
+import houseData2 from "./data/houseData2.json";
 
 function App() {
+  const [selectedAddress, setSelectedAddress] = useState<string>("");
+  const places = houseData;
+  const places2 = houseData2;
+
   return (
     <>
-      <h1>hello world</h1>
+      <HouseMap
+        places={places}
+        places2={places2}
+        selectedAddress={selectedAddress}
+      />
     </>
   );
 }
