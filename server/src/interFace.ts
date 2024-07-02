695ceb1a25ff01e07966d6bf16e2a2e4fa7c5aa6
Taejeong <mkiz0403@gmail.com>
2024-07-02 14:15:42 +0900

    node house2 인터페이스 추가

---
+++ server/src/interFace.ts
@@ -0,0 +1,23 @@
+export interface HouseInfo2 {
+  __EMPTY: number;
+  주소: string;
+  동: string | undefined;
+  호: number;
+  "주택군 이름": string;
+  주택열람일정: string;
+  "전용\r\n면적": number;
+  "주거공용\r\n면적": number;
+  면적계: number;
+  방수: number;
+  층수: string;
+  "승강기\r\n유무": string;
+  주택유형: string;
+  임대보증금: number;
+  월임대료: number;
+  임대보증금_1: number;
+  월임대료_1: number;
+  임대보증금_2: number;
+  월임대료_2: number;
+  임대보증금_3: number;
+  월임대료_3: number;
+}
